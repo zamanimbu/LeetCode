951c38cb99fbcb73d3b0bf125b4a1f1d0e8747c7
MD Ashraf Uz Zaman Shahria <itsashrafshahria@gmail.com>
2022-06-10 02:35:17 +0600

    finished

---
+++ ReverseWordInString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            Console.WriteLine(ReverseWordInString(inputString));
+            Console.ReadKey();
+        }
+
+        private static string ReverseWordInString(string inputString)
+        {
+            string removedSpace = string.Join(" ", inputString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            List<string> listWord = removedSpace.Split(' ').ToList();
+
+            return String.Join(" ", listWord.ToArray().Reverse());
+        }
+    }
+}
