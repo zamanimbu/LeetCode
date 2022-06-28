e9d04e6b45765e4dd3425fee2d811aa67ea1f232
MD Ashraf Uz Zaman Shahria <41391548+zamanimbu@users.noreply.github.com>
2022-06-28 18:19:09 +0600

    first attempt

--- StringToInteger/Program.cs
+++ StringToInteger/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System.Text;
+
+string? stringValue = Console.ReadLine();
+
+Console.WriteLine(MyAtoi(stringValue)); 
+Console.ReadLine();
+
+
+ static int MyAtoi(string? stringValue)
+{
+    string removedSpace = string.Join(" ", stringValue.Split(new char[] { ' ' }, 
+        StringSplitOptions.RemoveEmptyEntries));
+    char[] array = removedSpace.ToCharArray();
+    StringBuilder sb = new StringBuilder(); 
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (Char.IsDigit(array[i]) || array[i]=='-')
+        {
+            sb.Append(array[i]);
+        }
+    }
+    int integerValue = Convert.ToInt32(sb.ToString());
+    return integerValue;
+}
